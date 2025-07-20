8a8736ff26e32f71bd4e88cf4f1beca7e323493d
AsyncRifat <ibrahim3rifat@gmail.com>
2025-07-21 02:23:43 +0600

    axios interceptor implement and create birebase token and set request axios interceptor

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import React from 'react';
+import useAuth from './useAuth';
+import { useNavigate } from 'react-router';
 
 const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -7,6 +8,41 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
+  const { user, logOut } = useAuth();
+  const token = user?.accessToken;
+  const navigate = useNavigate();
+
+  // Add a request interceptor
+  axiosSecure.interceptors.request.use(
+    config => {
+      config.headers.authorization = `Bearer ${token}`;
+
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  );
+
+  // Add a response interceptor
+  axiosSecure.interceptors.response.use(response => {
+    return response;
+  });
+  error => {
+    if (error.status === 403) {
+      navigate('/forbidden');
+    } else if (error.status === 401) {
+      logOut()
+        .then(() => {
+          console.log(`sign out user for ${error.status} status code`);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+    return Promise.reject(error);
+  };
+
   return axiosSecure;
 };
 
