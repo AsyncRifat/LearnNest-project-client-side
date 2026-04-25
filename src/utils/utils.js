72502b127067b9c58e372b367afafbc438d19059
AsyncRifat <ibrahim3rifat@gmail.com>
2026-04-25 22:44:41 +0600

    polish

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 import useAxios from '../hooks/useAxios';
 import useAxiosSecure from '../hooks/useAxiosSecure';
 
+// image upload
 export const imageUpload = async fromImage => {
   const formImageData = new FormData();
   formImageData.append('image', fromImage);
 
   const { data } = await axios.post(
     `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMBB_API_KEY}`,
-    formImageData
+    formImageData,
   );
 
   return data?.data?.display_url;
